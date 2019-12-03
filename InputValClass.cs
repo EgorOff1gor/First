d6593682126f7d02c9cdd92df8ac1cb4f20a6ce5
Igor <egorov.flex.igor@gmail.com>
2019-12-03 21:34:52 +0200

    changed

--- InputValClass.cs
+++ InputValClass.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿dasdusing System;
+using Sasfaystem.Collections.Generic;
+using Systeasdm.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
