599bc82e7bf1ab297645b6aee799949556568c9f
EgorOff1gor <58484991+EgorOff1gor@users.noreply.github.com>
2019-12-03 21:33:14 +0200

    Update InputValClass.cs

--- InputValClass.cs
+++ InputValClass.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Sysasdastem;
+using Sysstem.Collections.Generic;
+using Sysctem.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
