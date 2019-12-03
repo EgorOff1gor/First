249765dcd16ce85a41bbb63eee749e2ef28e42c2
Igor <egorov.flex.igor@gmail.com>
2019-12-03 19:51:02 +0200

    ksdfvjbskdb

---
+++ InputValClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputValidation
+{
+    public static class InputValClass
+    {
+        public static int InputValidation()
+        {
+            
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("The inserted data is not a number! Please try again.");
+            }
+            return result;
+        }
+    }
+}
